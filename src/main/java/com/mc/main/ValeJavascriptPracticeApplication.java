c55d75576ee6fdc965255382bea5cdbbf8bea163
Alex Davies <MorickClive@Gmail.com>
2022-05-10 11:30:31 +0100

    AD: added springboot/tomcat hosting for index.html

---
+++ src/main/java/com/mc/main/ValeJavascriptPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.mc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValeJavascriptPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValeJavascriptPracticeApplication.class, args);
+	}
+
+}
